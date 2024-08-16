afe8104b6a732cab50aeb2db55ab8fe9da7f2243
oanhnk33 <oanhnk33@gmail.com>
2024-08-16 22:10:34 +0700

    Add files via upload

---
+++ Data Cleaning.sql
@@ -0,0 +1,13 @@
+-- Data cleaning
+-- Delete duplicate rows
+WITH CTE AS (
+    SELECT 
+        *,
+        ROW_NUMBER() OVER (PARTITION BY ride_id ORDER BY (SELECT NULL)) AS row_num
+    FROM 
+        data_2023
+)
+DELETE FROM CTE
+WHERE row_num > 1
+
+
